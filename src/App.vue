49f4560992875e411d22f1063f0bc0868c02c77f
MarGrzy <marcin.grzymowicz94@gmail.com>
2020-08-05 21:40:57 +0200

    Main App component has been flattened. Header and PlayersTable views have been added to App component. Committed vue.config.js file.

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,25 @@
 <template>
   <v-app id="main" :style="{background: $vuetify.theme.themes[theme].background}">
-    <v-app-bar
-        app
-        color="yellow darken-4"
-        dark
-    >
-      <v-app-bar-nav-icon>
-        <v-icon large>mdi-basketball</v-icon>
-      </v-app-bar-nav-icon>
-      <v-toolbar-title> NBA Players Application</v-toolbar-title>
-      <v-spacer/>
-      <v-switch
-          v-model="$vuetify.theme.dark"
-          hide-details
-          inset
-          color="grey lighten-1"
-          label="Light/Dark Theme"
-      ></v-switch>
-    </v-app-bar>
-    <v-content>
-      <div v-if="loggedUsername">
-        <v-toolbar class="elevation-0"
-                   dense>
-          <v-icon>mdi-account</v-icon>
-          <v-toolbar-title class="text-h6 grey--text text--darken-2">Welcome {{ loggedUsername }}</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn
-              color="primary"
-              @click="logout"
-          >Log Out
-          </v-btn>
-        </v-toolbar>
-      </div>
-      <div v-else>
-        <v-tabs v-model="tab" centered optional
-        >
-          <v-tab
-              v-for="item in items"
-              :key="item.tab"
-          >
-            {{ item.tab }}
-          </v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="tab">
-          <v-tab-item
-              v-for="item in items"
-              :key="item.tab"
-          >
-            <component v-bind:is="item.item"></component>
-          </v-tab-item>
-        </v-tabs-items>
-      </div>
-    </v-content>
+    <Header/>
+    <PlayersTable/>
   </v-app>
 </template>
 
 <script>
 
-import Login from './components/auth/Login.vue'
-import Registration from "@/components/auth/Registration";
-import {mapState} from 'vuex';
+import Header from "@/components/homepage/Header";
+import PlayersTable from "@/components/content/PlayersTable";
 
 export default {
   name: 'app',
   components: {
-    Registration,
-    Login
-  },
-  mounted() {
-    this.$store.dispatch("loadCurrentUser")
+    PlayersTable,
+    Header
   },
   computed: {
     theme() {
       return (this.$vuetify.theme.dark) ? 'dark' : 'light'
     },
-    ...mapState({
-      loggedUsername: state => state.data.loggedUsername
-    })
-  },
-  data: () => ({
-    tab: null,
-    items: [
-      {tab: 'Log in', item: 'Login'},
-      {tab: 'Sign up', item: 'Registration'}
-    ],
-  }),
-  methods: {
-    logout() {
-      this.$store.dispatch("logout")
-    }
-  },
+  }
 };
 </script>
